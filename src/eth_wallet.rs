5870ac60188676d11a29c0c9c826e9a65b236c13
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-13 16:31:13 -0500

    printing wallet to console

--- src/eth_wallet.rs
+++ src/eth_wallet.rs
@@ -1,9 +1,32 @@
+use anyhow::{bail, Result};
 use secp256k1::{
     rand::{rngs, SeedableRng},
     PublicKey, SecretKey,
 };
+use serde::{Deserialize, Serialize};
+use std::io::BufWriter;
+use std::str::FromStr;
+use std::{fs::OpenOptions, io::BufReader};
 use tiny_keccak::keccak256;
 use web3::types::Address;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Wallet {
+    pub secret_key: String,
+    pub public_key: String,
+    pub public_address: String
+}
+
+impl Wallet {
+    pub fn new(secret_key: &SecretKey, public_key: &PublicKey) -> Self {
+        let addr: Address = public_key_address(&public_key);
+        Wallet {
+            secret_key: secret_key.to_string(),
+            public_key: public_key.to_string(),
+            public_address: format!("{:?}", addr),
+        }
+    }
+}
 
 pub fn generate_keypair() -> (SecretKey, PublicKey) {
     let secp = secp256k1::Secp256k1::new();
