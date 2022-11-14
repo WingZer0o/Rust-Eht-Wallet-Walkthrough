66dcf05c44a22fff55bc65dafca529036b9e5265
Mike Mulchrone <mulchronemike0191@gmail.com>
2022-11-14 18:28:13 -0500

    env wallet

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod eth_wallet;
 
 fn main() {
+    dotenv::dotenv().ok();
     let (secret_key, pub_key) = eth_wallet::generate_keypair();
 
     println!("secret key: {}", &secret_key.to_string());
